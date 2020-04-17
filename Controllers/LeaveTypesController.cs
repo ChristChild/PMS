f0b3a2c10f24d7a54dff33142df32d07aee886b5
Trecia Thompson <trecia.thompson@hotmail.com>
2020-04-16 22:42:32 -0500

    Setup user auth, roles and modify registration process

--- Controllers/LeaveTypesController.cs
+++ Controllers/LeaveTypesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PMS.Contracts;
@@ -11,6 +12,7 @@ using PMS.Models;
 
 namespace PMS.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class LeaveTypesController : Controller
     {
         private readonly ILeaveTypeRepository _repo;
